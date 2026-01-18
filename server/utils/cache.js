27c519b1d2e89ef6f289ca30de7746f4b04dbc3f
Malinda <greshanamarakoon@gmail.com>
2026-01-18 10:42:24 +0530

    comment added

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,6 +1,6 @@
 const NodeCache = require("node-cache");
 
-// Standard TTL 300 seconds (5 minutes) as per requirements
+// Standard TTL(Time to Live) 300 seconds (5 minutes) as per requirements
 const cache = new NodeCache({ stdTTL: 300 });
 
 module.exports = cache;
