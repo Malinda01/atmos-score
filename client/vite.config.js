b09a93f60e4c995590ac64b190d694f5d751c56a
Malinda <greshanamarakoon@gmail.com>
2026-01-19 10:12:55 +0530

    dark mode and light mode done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite"; //
+import tailwindcss from "@tailwindcss/vite"; // <--- 1. Import this
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), //
+    tailwindcss(), // <--- 2. Add this to the plugin list
   ],
 });
